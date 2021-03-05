5ed2be72e6fe863025b8a0898a7875f8946c4b7c
Babitk21 <77804801+Babitk21@users.noreply.github.com>
2021-03-05 12:39:39 +0530

    obj pooler

--- Assets/ObjectPooler.cs
+++ Assets/ObjectPooler.cs
@@ -4,53 +4,58 @@ using UnityEngine;
 
 public class ObjectPooler : MonoBehaviour
 {
-    [System.Serializable]
-    public class GalaxyPool
-    {
-        public string name;
-        public GameObject obj;
-        public int size;
-    }
-    
-    public Dictionary<string, Queue<GameObject>> galaxyPool;
-    public List<GalaxyPool> galaxyPools;
+    #region singelton class
+    public static ObjectPooler Instance;
 
-    public static ObjectPooler instance;
     private void Awake()
     {
-        instance = this;
-    }
-
-    private void Start()
-    {
-        galaxyPool = new Dictionary<string, Queue<GameObject>>();
-
-        foreach(GalaxyPool pool in galaxyPools)
+        if (Instance == null)
         {
-            Queue<GameObject> pooledobjs = new Queue<GameObject>();
+            Instance = this;
+        }
+    }
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> bulletPoolDictionary;
+
+
+    #endregion
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletPoolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach(Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
             for(int i = 0; i < pool.size; i++)
             {
-                GameObject obj = Instantiate(pool.obj);
+                GameObject obj = Instantiate(pool.prefab);
                 obj.SetActive(false);
-                pooledobjs.Enqueue(obj);
+                objectPool.Enqueue(obj);
             }
-            galaxyPool.Add(pool.name, pooledobjs);
+            bulletPoolDictionary.Add(pool.tag, objectPool);
         }
+        
     }
-    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    public GameObject SpawnFromPool(string tag,Vector3 position, Quaternion rotation)
     {
-        if (!galaxyPool.ContainsKey(tag))
+        if (!bulletPoolDictionary.ContainsKey(tag))
         {
-            Debug.LogError("Pool tag Not Found");
+            Debug.LogWarning("pool with tag" + tag + " doesn't exist");
             return null;
         }
-        GameObject objToSpawn = galaxyPool[tag].Dequeue();
-        objToSpawn.SetActive(true);
-        objToSpawn.transform.position = position;
-        objToSpawn.transform.rotation = rotation;
-        galaxyPool[tag].Enqueue(objToSpawn);
-        return objToSpawn;
+        GameObject objectToSpawn = bulletPoolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        bulletPoolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
     }
 }
-
-
